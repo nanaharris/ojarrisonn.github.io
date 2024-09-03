c2e77be3a37af1f9d9bf729b8f1d32664cdfbc76
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-09-03 11:14:58 -0300

    core: removed references to Manuel

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,4 @@ export const SITE_DESCRIPTION =
 	"Welcome to my personal blog";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
+export const CONTACT_EMAIL = "harrisonn2004@gmail.com";
