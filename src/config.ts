c5d758f9ca27b629c726ddd4c88d2aadf250fb75
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-09-03 12:21:53 -0300

    format

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Harrisonn";
-export const SITE_DESCRIPTION =
-	"Welcome to my personal blog";
+export const SITE_DESCRIPTION = "Welcome to my personal blog";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
 export const CONTACT_EMAIL = "harrisonn2004@gmail.com";
