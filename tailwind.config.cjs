85012ccae9363c58346807f1c3160db52224d220
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-09-03 10:38:35 -0300

    style: use catppuccin theme
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,25 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: [
+			"light", "dark", "night",
+			{
+				"catppuccin": {
+					"primary": "#cba6f7",
+					"secondary": "#f5c2e7",
+					"accent": "#cba6f7",
+					"neutral": "#313244",
+					"base-100": "#1e1e2e",
+					"info": "#7ac7ec",
+					"success": "#a6e3a1",
+					"warning": "#fab387",
+					"error": "#f38ba8",
+				},
+			}
+		], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+	}
 }
